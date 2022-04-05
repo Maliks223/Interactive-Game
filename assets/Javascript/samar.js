5097aa0360b23ab401bff6e65e3b0ada3d804a6c
Samar Harbieh <samarharbieh.sh@gmail.com>
2022-04-05 20:49:12 +0000

    Fixing the playAudio function

--- assets/Javascript/samar.js
+++ assets/Javascript/samar.js
@@ -38,7 +38,18 @@ document.addEventListener("click", () => {
 
 function playAudio(audioId) {
   let audio = document.getElementById(audioId);
-  audio.play();
+if (typeof audio.loop == 'boolean')
+{
+    audio.loop = true;
+}
+else
+{
+    audio.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+}
+audio.play();
 }
 
 function stopAudio(audioId) {
